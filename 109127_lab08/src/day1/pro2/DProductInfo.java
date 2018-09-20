632b3a57ccbdf10df2b135be7637a755cd080a03
109127 <109127@CS_Lab115-204.cs.mum.edu>
2018-09-20 15:20:50 -0500

    sort

--- 109127_lab08/src/day1/pro2/DProductInfo.java
+++ 109127_lab08/src/day1/pro2/DProductInfo.java
@@ -6,21 +6,18 @@ import java.util.List;
 import day1.pro2.CProductInfo.SortMethod;
 
 public class DProductInfo {
-//	static enum SortMethod {
-//		BY_PRICE, BY_TITLE
-//	}
 
 	public void sort(List<Product> pros, final SortMethod method) {
-//		Collections.sort(pros, (p1,  p2) -> {
-//			if (p1.title.equalsIgnoreCase(p2.title)) {
-//				if (p1.model == p2.model)
-//					return 0;
-//				if (p1.model < p2.model)
-//					return -1;
-//				return 0;
-//			}
-//			return p1.title.compareToIgnoreCase(p2.title);
-//		});
+		Collections.sort(pros, (p1,  p2) -> {
+			if (p1.title.equalsIgnoreCase(p2.title)) {
+				if (p1.model == p2.model)
+					return 0;
+				if (p1.model < p2.model)
+					return -1;
+				return 0;
+			}
+			return p1.title.compareToIgnoreCase(p2.title);
+		});
 
 		pros.sort((p1, p2) -> {
 			if (method == SortMethod.BY_PRICE) {
